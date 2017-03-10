c673313a52587241efdf65e3d438d10346000fbe
heyulin <heyulin@smalltreemedia.com>
2017-03-10 17:40:16 +0800

    auto commit

---
+++ url.py
@@ -0,0 +1,7 @@
+"""
+encodeURI(str)           |  urllib.quote(str, safe='~@#$&()*!+=:;,.?/\'');
+-----------------------------------
+encodeURIComponent(str)  |  urllib.quote(str, safe='~()*!.\'')
+
+"""
+
