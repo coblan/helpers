2e88148a87880e80c7e369e4915190280f033c6e
heyulin <heyulin@hotmail.com>
2017-04-08 00:33:49 +0800

    增加 director model_validator.py

---
+++ director/model_validator.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import re
+
+def int_str(value):
+    if value and not re.match('^\d+$',value.strip()):
+        raise ValidationError(
+            _('%(value)s is not integer number'),
+            params={'value': value},
+        )
