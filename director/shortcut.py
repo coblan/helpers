f6b013bb275748e928e0c540835dd099ca797807
heyulin <heyulin@hotmail.com>
2017-04-27 01:39:28 +0800

    auto commit

--- director/shortcut.py
+++ director/shortcut.py
@@ -5,6 +5,7 @@ from .model_admin.base import model_dc,page_dc,render_dc,permit_list
 from .pages import TablePage,FormPage
 from .model_admin.tabel import ModelTable,PageNum,TrivalPageNum,RowFilter,RowSearch,RowSort
 from .model_admin.fields import ModelFields,save_row
-from .model_admin.permit import ModelPermit
+from .model_admin.permit import ModelPermit,has_permit
 from .container import evalue_container
+
 
