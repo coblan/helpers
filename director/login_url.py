34a56091922e4038f0f8eadb5a9f887372ee64e5
heyulin <heyulin@smalltreemedia.com>
2017-04-26 17:44:14 +0800

    auto commit

--- director/login_url.py
+++ director/login_url.py
@@ -4,6 +4,6 @@ from model_admin import render
 urlpatterns = [
     url(r'^login/?$',views.login,name='login'),
     url(r'^regist/?$',views.regist_user,name='regist'),
-    url(r'^logout/?$',views.logout),
+    url(r'^logout/?$',views.logout,name='logout'),
     url(r'^pswd/?$',views.change_pswd,name='change_password'),
     ]
