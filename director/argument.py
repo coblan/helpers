71f511311da76c48711d97dcad8fffa12b7f6271
heyulin <heyulin@hotmail.com>
2017-05-09 00:32:04 +0800

    auto commit

--- director/argument.py
+++ director/argument.py
@@ -4,27 +4,34 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 import urlparse
+from .cls import DotObj
+import re
+import json
 
 def get_argument(request):
     """
-    
     """
     if request.method=='POST':
-        dc =urlparse.parse_qs(request.body)
+        if re.match('{.+}|\[.+\]',request.body):
+            dc=json.loads(request.body)
+        else:
+            dc =urlparse.parse_qs(request.body)
         for k,v in dc.items():
             if isinstance(v,list):
                 dc[k]=v[0]
-        return dc
+        return DotObj( dc )
     else:
-        return request.GET.dict()
+        return DotObj( request.GET.dict() )
 
 def validate_argument(dc,validate_dict):
+    if isinstance(dc,DotObj):
+        dc=dc.__dict__
     for k,v in validate_dict.items():
         value = dc.get(k)
         for validator in v:
             value=validator(value,k)
         dc[k]=value
-    return dc
+    return DotObj( dc )
 
 
 def not_null(value,name):
@@ -33,10 +40,12 @@ def not_null(value,name):
     return value
 
 
-def model_instance(model):
+def model_instance(model,field='pk'):
     def _model_validator(value,name):
         try:
-            value = model.objects.get(pk=value)
+            if value:
+                dc={field:value}
+                return model.objects.get(**dc)
         except model.DoesNotExist as e:
             raise UserWarning,'%(name)s=%(value)s can not be find'%{'name':name,'value':value}
     return _model_validator
@@ -59,7 +68,6 @@ def default(def_value):
 
 def int_str(value,name):
     try:
-        value=int(value)
+        return int(value)
     except ValueError as e:
         raise UserWarning,'%(name)s=%(value)s counld not be covert to int'%{'name':name,'value':value}
-    return value
--- director/decorator.py
+++ director/decorator.py
@@ -1,7 +1,11 @@
 # encoding:utf-8
+from __future__ import unicode_literals
 
 from django.core.exceptions import PermissionDenied
 from .model_admin.permit import has_permit
+from django.http import HttpResponse
+import json
+from django.core.exceptions import ObjectDoesNotExist
 
 def need_login(fun):
     def _fun(request,*args,**kw):
@@ -18,4 +22,17 @@ def need_permit(fun):
             return fun(request,*args,**kw)
         else:
             raise PermissionDenied('need login ! If you access with session_id,it is mainly because session_id has been destroyed,this is happen when login at same device with diffrent accounts,the older session_id will be destroyed')
+    return _fun
+
+def warn_free(fun):
+    def _fun(request,*args,**kw):
+        try:
+            dc= fun(request,*args,**kw)
+        except (UserWarning,ObjectDoesNotExist) as e:
+            dc= {
+                'status':'fail',
+                'msg':unicode(e)
+            }
+        
+        return HttpResponse(json.dumps(dc),content_type="application/json")
     return _fun
