d9710361a5e5ccdf0f5549c287d37cdd4bda3fdd
coblan <coblan163.com>
2017-06-23 23:55:58 +0800

    sg

--- case/work/menu.py
+++ case/work/menu.py
@@ -17,6 +17,8 @@ def can_create_work(user):
     return permit.can_add()
 
 def can_read_all(request):
+    if request.user.is_anonymous:
+        return False
     permit = WorkReadValidDepart(request)
     allow_depart = permit.get_query_depart()
     if allow_depart:
