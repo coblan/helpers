0299c4f039595264d6d97e6ff9ef80d62dd0f32e
heyulin <coblan@163.com>
2017-07-30 21:25:35 +0800

    auto commit

---
+++ director/js/main.js
@@ -0,0 +1,10 @@
+import * as base from './base.js'
+import * as f7 from './f7.js'
+ function version(){
+     var ssssssss='jjyy'
+    alert('v1.0')
+     alert(ssssssss)
+}
+
+window.ver=version
+window.base=base
