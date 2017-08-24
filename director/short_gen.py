a793373c3acc1b5b0205971492e8e08b436ec849
heyulin <heyulin@smalltreemedia.com>
2017-08-24 18:57:51 +0800

    auto commit

--- director/short_gen.py
+++ director/short_gen.py
@@ -1,9 +1,20 @@
+# encoding:utf-8
+
+from __future__ import unicode_literals
+
 from .model_admin.fields import ModelFields
 from .model_admin.tabel import ModelTable
 from .pages import FormPage,TablePage
 from .model_admin.base import model_dc,permit_list,page_dc
 
-def regist_director(prefix_list,src_model):
+def regist_director(name='',src_model=None,prefix_list=[]):
+    """
+    name和prefix_list二选一，有name时优先用name
+    @name
+    @prefix_list : 名字列表
+    
+    之所以用prefix_list，因为有时需要生成pc和移动两个 model manage 页面，这是就需要传入pc和移动的 prefix list，会生成两个
+    """
     class ThisForm(ModelFields):
         class Meta:
             model=src_model
@@ -19,9 +30,13 @@ def regist_director(prefix_list,src_model):
         tableCls=ThisTable
     
     dc={}
-    for prefix in prefix_list:
-        dc[prefix]=ThisTablePage
-        dc[prefix+'.edit']=ThisFormPage
+    if name:
+        dc[name]=ThisTablePage
+        dc[name+'.edit']=ThisFormPage
+    else:
+        for prefix in prefix_list:
+            dc[prefix]=ThisTablePage
+            dc[prefix+'.edit']=ThisFormPage
     
     model_dc[src_model]={'fields':ThisForm}
     permit_list.append(src_model)
