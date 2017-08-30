730ce0770ec24678b64497cf2a9dc421a047f876
heyulin <heyulin@smalltreemedia.com>
2017-08-30 12:12:12 +0800

    auto commit

--- maintenance/update_static_timestamp.py
+++ maintenance/update_static_timestamp.py
@@ -20,7 +20,11 @@ def get_static_obj(path):
 org_static_file_path=os.path.join(settings.BASE_DIR,'static')
 volatile_static_file_path=os.path.join(org_static_file_path,'js')
 
+# 这个名字太长了
 static_file_timestamp_dict=get_static_obj(volatile_static_file_path)
+
+# 下面这个名字短点
+js_stamp=get_static_obj(volatile_static_file_path)
 
 
 
