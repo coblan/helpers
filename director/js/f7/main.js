a1ad87ee252f70362e02544b184620ea4dfb5a41
coblan <coblan163.com>
2017-10-19 22:53:40 +0800

    auto commit

--- director/js/f7/main.js
+++ director/js/f7/main.js
@@ -4,7 +4,7 @@ import * as filter_win from './filter_win.js'
 import {table_time_group} from  './table_time_group.js'
 import {popup_page} from  './popup.js'
 
-import  * as mb_jianrong from  './mb_jianrong.js'
+import  {stop_error_Drop} from  './mb_jianrong.js'
 
 import * as try01 from './try.js'
 
@@ -14,3 +14,7 @@ window.table_time_group=table_time_group
 window.popup_page=popup_page
 
 window.ff=f7.ff
+
+$(function(){
+    stop_error_Drop()
+})
