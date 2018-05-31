375ec86a1c8978751662b7bfa1900b1adaa5dc6a
he_yulin <he_yulin@163.com>
2018-05-31 20:03:45 +0800

    增加decimal json化

--- director/data_format/json_format.py
+++ director/data_format/json_format.py
@@ -1,13 +1,14 @@
 import json  
 from django.utils import timezone
 from datetime import datetime,date
-
+from decimal import Decimal
 class DirectorEncoder(json.JSONEncoder):  
     def default(self, obj):  
         if isinstance(obj, (timezone.datetime,datetime) ):  
             return obj.strftime('%Y-%m-%d %H:%M:%S')  
         elif isinstance(obj, date):  
             return obj.strftime("%Y-%m-%d")  
-        
+        elif isinstance(obj, Decimal):
+            return str(obj)
         else:  
             return json.JSONEncoder.default(self, obj) 
