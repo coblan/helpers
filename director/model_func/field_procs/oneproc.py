20fe7e0c279a71be325242e66eda965c3872d986
he_yulin <he_yulin@163.com>
2018-06-28 17:51:26 +0800

    debug

--- director/model_func/field_procs/oneproc.py
+++ director/model_func/field_procs/oneproc.py
@@ -2,23 +2,27 @@
 from ..field_proc  import BaseFieldProc
 from django.db.models import OneToOneField
 from .. .base_data import field_map
-from ..dictfy import model_to_name
-
+from ..dictfy import model_to_name, name_to_model
 
 class OneProc(BaseFieldProc):
     def to_dict(self,inst,name):
+        
         foreign=getattr(inst,name,None)
         if foreign:
             return {
                 name: foreign.pk ,
-                '_%s_model'%name:model_to_name(foreign.__class__)
+                '_%s_model'%name:model_to_name(foreign.__class__), 
+                '_%s_label'%name:str(foreign), 
             }
         else:
-            return {}
+            related_model = inst.__class__._meta.get_field(name).related_model
+            return {
+                '_%s_model'%name:model_to_name(related_model)
+            }
     
-    def clean_field(self,dc,name):
-        model = name_to_model( dc.get('_%s_model'%name) )
-        return model.objects.get(pk=dc.get(name))  
+    #def clean_field(self,dc,name):
+        #model = name_to_model( dc.get('_%s_model'%name) )
+        #return model.objects.get(pk=dc.get(name))  
     
     
 field_map.update({
