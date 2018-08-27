0382ff2907bb051dbf27aebfba9d290d87620628
he_yulin <he_yulin@163.com>
2018-08-27 13:27:32 +0800

    debug

--- case/jb_admin/js/mix/mix_nice_validator.js
+++ case/jb_admin/js/mix/mix_nice_validator.js
@@ -6,33 +6,36 @@
 
 var nice_validator={
     mounted:function(){
-        var self=this
-        var validator={}
-        ex.each(this.heads,function(head){
-            var ls=[]
-
-            if(head.fv_rule){
-                ls.push(head.fv_rule)
-            }
-            if( head.required){
-                if(!head.fv_rule || head.fv_rule.search('required')==-1){// 规则不包含 required的时候，再添加上去
-                    ls.push('required')
-                }
-            }
-            validator[head.name]=ls.join(';')
-        })
-        if($(this.$el).hasClass('field-panel')){
-            this.nice_validator =$(this.$el).validator({
-                fields: validator,
-            });
-        }else{
-            this.nice_validator =$(this.$el).find('.field-panel').validator({
-                fields: validator,
-            });
-        }
+        this.update_nice()
 
     },
     methods:{
+        update_nice:function(){
+            var self=this
+            var validator={}
+            ex.each(this.heads,function(head){
+                var ls=[]
+
+                if(head.fv_rule){
+                    ls.push(head.fv_rule)
+                }
+                if( head.required){
+                    if(!head.fv_rule || head.fv_rule.search('required')==-1){// 规则不包含 required的时候，再添加上去
+                        ls.push('required')
+                    }
+                }
+                validator[head.name]=ls.join(';')
+            })
+            if($(this.$el).hasClass('field-panel')){
+                this.nice_validator =$(this.$el).validator({
+                    fields: validator,
+                });
+            }else{
+                this.nice_validator =$(this.$el).find('.field-panel').validator({
+                    fields: validator,
+                });
+            }
+        },
         isValid:function(){
             var nice_rt = this.nice_validator.isValid()
             //var totalValid=[nice_rt]
