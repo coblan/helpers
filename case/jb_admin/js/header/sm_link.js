d458a280b6ec76812f6b1b1cd4e632f2241d332d
he_yulin <he_yulin@163.com>
2018-10-16 01:10:49 +0800

    改进

--- case/jb_admin/js/header/sm_link.js
+++ case/jb_admin/js/header/sm_link.js
@@ -1,15 +1,13 @@
-//require('./scss/dropdown.scss')
+require('./scss/sm_link.scss')
 
-
+// header 上的小链接
 Vue.component('com-head-sm-link',{
     props:['head'],
-    template:`<div>
-    <span v-for="action in head.options">
+    template:`<div class="small-link">
+    <span class="item" v-for="action in head.options">
         <a :href="action.url" class="login-link" v-text="action.label"></a>
-        <span v-if="action != head.options[head.options.length-1]">&nbsp;|&nbsp;</span>
+        <span class="space" v-if="action != head.options[head.options.length-1]">&nbsp;|&nbsp;</span>
     </span>
-
-        <!--<a href="/accounts/regist">注册</a>-->
 
     </div>`
 })
