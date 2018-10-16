8011d79d0cb90ef7eae2c31b34b78811c3a927ed
he_yulin <he_yulin@163.com>
2018-10-17 00:28:01 +0800

    debug

--- case/jb_admin/js/header/sm_link.js
+++ case/jb_admin/js/header/sm_link.js
@@ -5,9 +5,17 @@ Vue.component('com-head-sm-link',{
     props:['head'],
     template:`<div class="small-link">
     <span class="item" v-for="action in head.options">
-        <a :href="action.url" class="login-link" v-text="action.label"></a>
+        <a href="jacascript::void(0)" @click="on_click(action.url)" class="login-link" v-text="action.label"></a>
         <span class="space" v-if="action != head.options[head.options.length-1]">&nbsp;|&nbsp;</span>
     </span>
-
-    </div>`
+    </div>`,
+    methods:{
+        on_click:function(){
+            if(this.$listeners && this.$listeners.jump){
+                this.$emit('jump',url)
+            }else{
+                location = url
+            }
+        }
+    }
 })
