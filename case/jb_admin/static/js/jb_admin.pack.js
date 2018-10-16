c1b846c5313833a401fb6732cf119900405e0351
he_yulin <he_yulin@163.com>
2018-10-16 13:57:33 +0800

    改进

--- case/jb_admin/static/js/jb_admin.pack.js
+++ case/jb_admin/static/js/jb_admin.pack.js
@@ -2729,7 +2729,12 @@ window.mix_v_table_adapter = mix_v_table_adapter;
 $.validator.config({
     rules: {
         mobile: [/^1[3-9]\d{9}$/, "请填写有效的手机号"],
-        chinese: [/^[\u0391-\uFFE5]+$/, "请填写中文字符"]
+        chinese: [/^[\u0391-\uFFE5]+$/, "请填写中文字符"],
+        digit: function digit(element, params) {
+            var digits = params[0];
+            var pattern = "\\.\\d{" + digits + "}$";
+            return RegExp(pattern).test(element.value) || '请确定有效位数为' + digits;
+        }
     }
 });
 
