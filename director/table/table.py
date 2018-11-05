977da98dcca7a6abf0bd473cf8f656aaf1bc400e
he_yulin <he_yulin@163.com>
2018-11-05 17:24:56 +0800

    debug

--- director/table/table.py
+++ director/table/table.py
@@ -624,8 +624,10 @@ class ModelTable(object):
         return {}
     
     def get_query(self):
-        if not self.crt_user.is_superuser and not self.permit.readable_fields():
-            raise PermissionDenied('no permission to browse %s'%self.model._meta.model_name)
+        if not self.crt_user.is_authenticated:
+            raise PermissionDenied('no permission to browse %s ,Please login first' % self.model._meta.model_name)
+        elif not self.crt_user.is_superuser and not self.permit.readable_fields():
+            raise PermissionDenied('user %s ,no permission to browse %s'% ( self.crt_user.username, self.model._meta.model_name))
         query =  self.model.objects.all()
         
         # 优化速度
