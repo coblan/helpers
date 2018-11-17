7e5bb5ead4f1d4b914957a9516fd10ee9e77ddf8
he_yulin <he_yulin@163.com>
2018-11-18 00:05:08 +0800

    debug

--- director/static/js/exfun.pack.js
+++ director/static/js/exfun.pack.js
@@ -829,7 +829,15 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var layout = exports.layout = {
-    stickup: function stickup(node) {
+    stickup: function stickup(node, options) {
+        /*
+        node: 需要stickup的元素
+        options:
+            dom: 滚动的容器元素。注意：滚动元素应该的父元素不能 **再滚动**了。
+            top：stickup 时距离上边缘的距离
+          * */
+        var dom = options.dom || window;
+        var fixed_top = options.top || 0;
         var $cur = $(node); //方便后面操作this。
         var top = $cur.offset().top; //获取元素距离顶部的距离
         var left = $cur.offset().left; //获取元素的水平位置
@@ -839,9 +847,9 @@ var layout = exports.layout = {
         //克隆这个元素，这里opactiy和display:none 是双重保险.
         var now = $cur.clone().css("opacity", 0).insertBefore($cur).hide();
 
-        $(window).on("scroll", function () {
+        $(dom).on("scroll", function () {
 
-            var socrllTop = $(window).scrollTop();
+            var socrllTop = $(dom).scrollTop();
             if (socrllTop >= top) {
                 setStick();
             } else {
@@ -850,11 +858,11 @@ var layout = exports.layout = {
         });
 
         function setStick() {
-            console.log($(window).scrollLeft());
+            console.log($(dom).scrollLeft());
             $cur.css({
                 "position": "fixed",
-                "left": left - $(window).scrollLeft(),
-                "top": 0,
+                "left": left - $(dom).scrollLeft(),
+                "top": fixed_top,
                 "width": width,
                 "height": height,
                 "z-index": 10
@@ -864,6 +872,53 @@ var layout = exports.layout = {
 
         function unsetStick() {
             $cur.removeAttr("style"), now.hide();
+        }
+    },
+    stickupNest: function stickupNest(options) {
+        // {slide:'xxx',node:'xxx'}
+        var slide = options.slide;
+        var node = options.node;
+        var $slide = $(slide);
+        var $cur = $(node); //方便后面操作this。
+
+        //var top = $cur.offset().top;//获取元素距离顶部的距离
+        var left = $cur.offset().left; //获取元素的水平位置
+        var width = $cur.width(); //获取元素的宽度
+        var height = $cur.height(); //获取元素的高度
+
+        //克隆这个元素，这里opactiy和display:none 是双重保险.
+        var now = $cur.clone().css("opacity", 0).insertBefore($cur).hide();
+
+        $slide.on("scroll", function () {
+            var par_top = $slide.offset().top;
+            //var slide_top = $slide.offset().top;
+            var cur_top = $cur.offset().top;
+            var socrllTop = $slide.scrollTop();
+            if (par_top >= cur_top) {
+                setStick(par_top);
+            } else {
+                unsetStick();
+            }
+        });
+
+        function setStick(top) {
+            //console.log($(dom).scrollLeft())
+            //$cur.addClass(cls)
+            $cur.css({
+                "position": "fixed",
+                "left": left - $slide.scrollLeft(),
+                "top": top,
+                "width": width,
+                "height": height,
+                "z-index": 10
+            });
+            now.show();
+        }
+
+        function unsetStick() {
+            $cur.removeAttr("style"),
+            //$cur.removeClass(cls)
+            now.hide();
         }
     }
 };
