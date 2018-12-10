eb1714d8359655c96532bd6748f9c9aae1ae54f6
he_yulin <he_yulin@163.com>
2018-12-10 20:10:11 +0800

    改进

--- case/jb_admin/js/nice_validator_rule.js
+++ case/jb_admin/js/nice_validator_rule.js
@@ -4,7 +4,7 @@ $.validator.config({
         chinese: [/^[\u0391-\uFFE5]+$/, "请填写中文字符"],
         digit: function(element, params) {
             var digits = params[0]
-            var pattern = `\\.\\d{${digits}}$`
+            var pattern = `\\.\\d{0,${digits}}$|^[\\d]+$`
             return RegExp(pattern).test(element.value) || '请确定有效位数为'+digits;
         },
         dot_split_int:function(element,params){
