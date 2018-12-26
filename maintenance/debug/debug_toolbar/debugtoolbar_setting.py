f065a6f632c88779176481a5065a146deefcb73a
he_yulin <he_yulin@163.com>
2018-12-26 14:55:44 +0800

    debug

--- maintenance/debug/debug_toolbar/debugtoolbar_setting.py
+++ maintenance/debug/debug_toolbar/debugtoolbar_setting.py
@@ -16,12 +16,33 @@ def SET(scope):
     scope['INSTALLED_APPS'].extend(['debug_toolbar',])
     scope['MIDDLEWARE_CLASSES'].extend([
         'debug_toolbar.middleware.DebugToolbarMiddleware',
-        #'helpers.maintenance.debug.debug_toolbar.json_as_html.JsonAsHTML',
-        
+        'helpers.maintenance.debug.debug_toolbar.json_as_html.JsonAsHTML',
         ])
-    scope['DEBUG_TOOLBAR_CONFIG'] =''
-    #scope['DEBUG_TOOLBAR_CONFIG'] = {  'JQUERY_URL' : r"/static/lib/jquery3.2.1.min.js"}
+    #scope['MIDDLEWARE'].extend( [
+        #'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #]   )
+ 
+    #scope['DEBUG_TOOLBAR_CONFIG'] ={}
+
     scope['INTERNAL_IPS'] =['127.0.0.1']
+    DEBUG_TOOLBAR_PANELS = [
+        'ddt_request_history.panels.request_history.RequestHistoryPanel',  # Here it is 
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    ]
+    scope['DEBUG_TOOLBAR_PANELS']=DEBUG_TOOLBAR_PANELS
+
 
 
 def AUTO_URL(scope):
