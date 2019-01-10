494e0fd69c94705d1b9ec03fe177c237439549c3
he_yulin <he_yulin@163.com>
2019-01-11 00:38:18 +0800

    debug

--- director/decorator.py
+++ director/decorator.py
@@ -5,13 +5,15 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 import json
 from django.core.exceptions import ObjectDoesNotExist
+from .middleware.request_cache import get_request_cache
 
 def need_login(fun):
-    def _fun(request,*args,**kw):
+    def _fun(*args,**kw):
+        request = get_request_cache().get('request')
         if request.user.is_authenticated():
-            return fun(request,*args,**kw)
+            return fun(*args,**kw)
         else:
-            raise PermissionDenied('need login ! If you access with session_id,it is mainly because session_id has been destroyed,this is happen when login at same device with diffrent accounts,the older session_id will be destroyed')
+            raise PermissionDenied('Need login !')
     return _fun
 
 def need_permit(fun):
