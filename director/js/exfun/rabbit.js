2f2ea664f9e9632565a033c73ea12cded3b85ddb
he_yulin <he_yulin@163.com>
2019-01-15 00:23:14 +0800

    debug

--- director/js/exfun/rabbit.js
+++ director/js/exfun/rabbit.js
@@ -7,42 +7,44 @@ export var rabbit={
         pswd:'guest',
     }
     * */
-    rabbit_regist:function(ctx,callback){
-
-        ex.load_js('https://cdn.bootcss.com/stomp.js/2.3.2/stomp.min.js',function(){
-            // Stomp.js boilerplate
-            //var client = Stomp.client('ws://' + rabbit.url + ':15674/ws');
-            var client = Stomp.client(ctx.url);
+    rabbit_regist:function(ctx){
+    var p =new Promise(function(resolve,reject) {
+            ex.load_js('https://cdn.bootcss.com/stomp.js/2.3.2/stomp.min.js', function () {
+                // Stomp.js boilerplate
+                //var client = Stomp.client('ws://' + rabbit.url + ':15674/ws');
+                var client = Stomp.client(ctx.url);
 //        client.debug = pipe('#second');
 
 //        var print_first = pipe('#first', function(data) {
 //            client.send('/topic/test', {"content-type":"text/plain"}, data);
 //        });
-            var on_connect = function(x) {
+                var on_connect = function (x) {
 //          id = client.subscribe("/topic/test", function(d) {
 //               print_first(d.body);
 //          });
 
-                //id = client.subscribe("/exchange/zhaoxiang_weilan_warning", function(d) {
-                //    layer.alert(d.body)
-                //
-                //    console.log(d.body)
-                //});
-                callback(client)
+                    //id = client.subscribe("/exchange/zhaoxiang_weilan_warning", function(d) {
+                    //    layer.alert(d.body)
+                    //
+                    //    console.log(d.body)
+                    //});
+                    resolve(client)
 
-            };
-            var on_error =  function(e) {
-                console.log('error');
-                console.log(e)
+                };
+                var on_error = function (e) {
+                    console.log('error');
+                    console.log(e)
 
-                setTimeout(function(){
-                    client = Stomp.client(ctx.url);
-                    client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
-                },10000)
+                    setTimeout(function () {
+                        client = Stomp.client(ctx.url);
+                        client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
+                    }, 10000)
 
-            };
-            client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
+                };
+                client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
+            })
         })
+        return p
 
     }
 }
--- director/static/js/exfun.pack.js
+++ director/static/js/exfun.pack.js
@@ -1723,40 +1723,42 @@ var rabbit = exports.rabbit = {
         pswd:'guest',
     }
     * */
-    rabbit_regist: function rabbit_regist(ctx, callback) {
+    rabbit_regist: function rabbit_regist(ctx) {
+        var p = new Promise(function (resolve, reject) {
+            ex.load_js('https://cdn.bootcss.com/stomp.js/2.3.2/stomp.min.js', function () {
+                // Stomp.js boilerplate
+                //var client = Stomp.client('ws://' + rabbit.url + ':15674/ws');
+                var client = Stomp.client(ctx.url);
+                //        client.debug = pipe('#second');
 
-        ex.load_js('https://cdn.bootcss.com/stomp.js/2.3.2/stomp.min.js', function () {
-            // Stomp.js boilerplate
-            //var client = Stomp.client('ws://' + rabbit.url + ':15674/ws');
-            var client = Stomp.client(ctx.url);
-            //        client.debug = pipe('#second');
+                //        var print_first = pipe('#first', function(data) {
+                //            client.send('/topic/test', {"content-type":"text/plain"}, data);
+                //        });
+                var on_connect = function on_connect(x) {
+                    //          id = client.subscribe("/topic/test", function(d) {
+                    //               print_first(d.body);
+                    //          });
 
-            //        var print_first = pipe('#first', function(data) {
-            //            client.send('/topic/test', {"content-type":"text/plain"}, data);
-            //        });
-            var on_connect = function on_connect(x) {
-                //          id = client.subscribe("/topic/test", function(d) {
-                //               print_first(d.body);
-                //          });
+                    //id = client.subscribe("/exchange/zhaoxiang_weilan_warning", function(d) {
+                    //    layer.alert(d.body)
+                    //
+                    //    console.log(d.body)
+                    //});
+                    resolve(client);
+                };
+                var on_error = function on_error(e) {
+                    console.log('error');
+                    console.log(e);
 
-                //id = client.subscribe("/exchange/zhaoxiang_weilan_warning", function(d) {
-                //    layer.alert(d.body)
-                //
-                //    console.log(d.body)
-                //});
-                callback(client);
-            };
-            var on_error = function on_error(e) {
-                console.log('error');
-                console.log(e);
-
-                setTimeout(function () {
-                    client = Stomp.client(ctx.url);
-                    client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
-                }, 10000);
-            };
-            client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
+                    setTimeout(function () {
+                        client = Stomp.client(ctx.url);
+                        client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
+                    }, 10000);
+                };
+                client.connect(ctx.user, ctx.pswd, on_connect, on_error, '/');
+            });
         });
+        return p;
     }
 };
 
