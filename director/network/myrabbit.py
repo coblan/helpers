de3b0fb16056b47a00f3a48191f75de1629080e7
he_yulin <he_yulin@163.com>
2019-01-14 19:00:53 +0800

    debug

--- director/network/myrabbit.py
+++ director/network/myrabbit.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 import pika
 
-rabbitChanel=None
+#rabbitChanel=None
 
-def rabbit_init():
+def get_rabbit_chanel():
     global rabbitChanel
     credentials = pika.PlainCredentials(settings.RABBIT_USER,settings.RABBIT_PSWD)
     connection =pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBIT_SERVER,credentials=credentials))
     rabbitChanel = connection.channel()
     return rabbitChanel
 
-rabbit_init()
+#rabbit_init()
