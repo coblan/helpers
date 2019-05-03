4c4cec189d0fa9e6cc81e7a051e1898ff6f6457d
he_yulin <he_yulin@163.com>
2019-05-04 02:23:33 +0800

    DEBUG

---
+++ director/middleware/tokenuser.py
@@ -0,0 +1,17 @@
+from threading import currentThread
+#from django.core.cache.backends.locmem import LocMemCache
+import json
+from django.utils.deprecation import MiddlewareMixin
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
+
+class TokenUser(MiddlewareMixin):
+
+    def process_request(self, request):
+        token = request.META.get('HTTP_TOKEN')
+        if token:
+            request.COOKIES['sessionid'] = token
+    
+
+        
+        
