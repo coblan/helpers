49aec04c8a195d6cd673b3d1fb839229aefbebdb
he_yulin <he_yulin@163.com>
2019-05-05 20:39:20 +0800

    debug

---
+++ case/jb_admin/js/field_editor/int.js
@@ -0,0 +1,26 @@
+var number = {
+    props:['row','head'],
+
+    template: `<div><span v-if='head.readonly' v-text='row[head.name]'></span>
+            		<input v-else type="text" class="form-control input-sm" v-model="row[head.name]" :id="'id_'+head.name"
+            		    :name="head.name" :step="head.step"
+            		     @keypress="isNumber($event)"
+                        :placeholder="head.placeholder" :autofocus="head.autofocus"></div>`,
+    methods:{
+        isNumber:function(evt){
+            evt = (evt) ? evt : window.event;
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            if ((charCode >= 48 && charCode <= 57) || charCode== 46) {
+                if(charCode==46 && this.row[this.head.name].indexOf('.')!=-1){
+                    return evt.preventDefault();
+                }else{
+                    return true
+                }
+            }else{
+                return evt.preventDefault();
+            }
+        }
+    }
+}
+
+Vue.component('com-field-int',number)
