1e67aae2999120f1fca941879cabdac88e4fd1d2
he_yulin <he_yulin@163.com>
2019-05-11 12:13:19 +0800

    debug

---
+++ mobile/js/field_editor/label_shower.js
@@ -0,0 +1,18 @@
+Vue.component('com-field-label-shower',{
+    props:['head','row'],
+    template:`<van-field class="com-field-label-shower"
+    v-model="row[head.name]"
+    :label="head.label"
+    type="text"
+    :name="head.name"
+    autosize
+    readonly
+  >
+  </van-field>`,
+    computed:{
+        label_text(){
+            return this.row['_'+this.head.name+'_label']
+        }
+    },
+})
+
