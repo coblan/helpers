8517f33d9bad8b216f767f6b4e667b70ea497319
he_yulin <he_yulin@163.com>
2019-10-22 17:31:25 +0800

    增加

--- case/jb_admin/js/field_editor/line_text.js
+++ case/jb_admin/js/field_editor/line_text.js
@@ -3,7 +3,7 @@ var line_text={
     template:`<div :class="['com-field-linetext',head.class]" :style="head.style">
             			<span class="readonly-info" v-if='head.readonly' v-text='row[head.name]'></span>
             			<input v-else type="text" :class="['form-control input-sm',head.input_class]" v-model="row[head.name]"
-            		 	    :id="'id_'+head.name" :name="head.name" data-
+            		 	    :id="'id_'+head.name" :name="head.name"
                         	:placeholder="head.placeholder" :autofocus="head.autofocus" :maxlength='head.maxlength'>
                        </div>`,
     mounted(){
