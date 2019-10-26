8c2928c634137f631f95e9f1f341bf69af14bfd4
he_yulin <he_yulin@163.com>
2019-10-26 15:33:31 +0800

    dd

--- director/model_func/cus_fields/form_datetime.py
+++ director/model_func/cus_fields/form_datetime.py
@@ -3,6 +3,7 @@ from ..field_procs.datetimeproc import DateTimeProc
 from .. .base_data import field_map
 
 class FormDatetime(DateTimeField):
+    """ 用于解决 auto_now auto_now_add 时，不能在modelform中显示"""
     def __init__(self, verbose_name=None, name=None, auto_now=False,
                      auto_now_add=False, **kwargs):
         self.auto_now, self.auto_now_add = auto_now, auto_now_add
