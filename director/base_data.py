a29b715ea659576eb7f753e9f6f3ef65df9e7a26
he_yulin <he_yulin@163.com>
2019-12-18 00:54:57 +0800

    增加文档生成功能

--- director/base_data.py
+++ director/base_data.py
@@ -1,3 +1,5 @@
+import inspect
+
 # used for model render
 page_dc={
     #'xxx_model': {'model':'xxx','table_temp':xxx,'field_temp':xxx}
@@ -27,8 +29,33 @@ inspect_dict ={
     'director_views':director_views,
 }
 
-
 from functools import wraps
+
+doc_dict = {}
+
+def doc_str(file_key,text,sort=100,tag=['api']):
+    if file_key not in doc_dict:
+        doc_dict[file_key] = []
+    doc_dict[file_key].append(
+        {'text':text,'sort':sort,'tag':tag}
+    )
+    
+
+def doc_fun(file_key,sort=100,tag=['api']):
+    if file_key not in doc_dict:
+        doc_dict[file_key] = []
+        
+    def _fun(fun): 
+        doc_dict[file_key].append(
+            {'text':fun.__doc__,'sort':sort,'tag':tag}
+        )
+        @wraps(fun)
+        def _fun2(*args, **kargs): 
+            return fun(*args, **kargs)
+        return _fun2
+    return _fun
+
+
 
 def director_view(name): 
     def _fun(fun): 
