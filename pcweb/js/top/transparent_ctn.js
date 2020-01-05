709dfefb79cddf95f14239efb0645e354f99042d
he_yulin <he_yulin@163.com>
2020-01-05 20:40:16 +0800

    改进

--- pcweb/js/top/transparent_ctn.js
+++ pcweb/js/top/transparent_ctn.js
@@ -6,11 +6,11 @@ Vue.component('com-top-transparent-ctn',{
     props:['ctx'],
     template:`<div class="com-top-transparent-ctn" :style="mystyle">
         <div class = 'web-wrap'>
-        <!--<div v-if="ctx.title" class="title" v-text="ctx.title"> </div>-->
-        <!--<div v-if="ctx.sub_title" class="sub-title" v-text="ctx.sub_title"></div>-->
-        <!--<div class="block-content">-->
-          <!--<component v-for="item in ctx.items" :is="item.editor" :ctx="item"></component>-->
-        <!--</div>-->
+            <div v-if="ctx.title" class="title" v-text="ctx.title"> </div>
+            <div v-if="ctx.subtitle" class="subtitle" v-text="ctx.subtitle"></div>
+            <div class="block-content">
+              <component v-for="item in ctx.items" :is="item.editor" :ctx="item"></component>
+            </div>
         </div>
     </div>`,
     computed:{
