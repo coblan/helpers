d63d361c2d48ca2baf22fc0d89ece11a433486af
he_yulin <he_yulin@163.com>
2020-03-04 00:01:51 +0800

    增加mobile - switch

---
+++ mobile/js/field_editor/switch.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="com-field-switch">
+        <van-cell :title="head.label" >
+            <van-switch v-model="checked"  active-color="#07c160" :disabled="head.readonly"/>
+        </van-cell>
+    </div>
+</template>
+<script>
+    export default{
+        props:['row','head'],
+        data(){
+            if(this.head.int_bool){
+                if(this.row[this.head.name]==1){
+                    var checked = true
+                }else{
+                    var checked = false
+                }
+            }else{
+                var checked = this.row[this.head.name]
+            }
+            return {
+                checked:checked
+            }
+        },
+        watch:{
+            checked(v){
+                if(this.head.int_bool){
+                    if(v){
+                        this.row[this.head.name] =1
+                    }else{
+                        this.row[this.head.name] = 0
+                    }
+                }else{
+                    this.row[this.head.name] = v
+                }
+            }
+        }
+    }
+</script>
