81fd046f593665ea91726877813a7b737540ccd9
he_yulin <he_yulin@163.com>
2020-03-31 00:11:43 +0800

    dd

---
+++ mobile/js/top/swiper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="com-top-swiper">
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+            <van-swipe-item v-for="item in ctx.items">
+                <img :src="item.image_url" alt="">
+            </van-swipe-item>
+        </van-swipe>
+    </div>
+</template>
+<script>
+    export default {
+        props:['ctx'],
+    }
+</script>
+
+<style scoped lang="scss">
+    .my-swipe .van-swipe-item {
+        img{
+            max-width: 100vw;
+            height: auto;
+        }
+        /*color: #fff;*/
+        /*font-size: 20px;*/
+        /*line-height: 150px;*/
+        /*text-align: center;*/
+        /*background-color: #39a9ed;*/
+    }
+</style>
