c6d41c0f1b51e456660282237b2cdea4993f4095
he_yulin <he_yulin@163.com>
2020-03-31 00:27:17 +0800

    改进

--- mobile/js/top/swiper.vue
+++ mobile/js/top/swiper.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="com-top-swiper">
+    <div class="com-top-swiper" :class="ctx.class">
         <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
             <van-swipe-item v-for="item in ctx.items">
-                <img :src="item.image_url" alt="">
+                <div class="img-container" :style="get_style(item.image_url)"></div>
+                <!--<img :src="item.image_url" alt="">-->
             </van-swipe-item>
         </van-swipe>
     </div>
@@ -10,15 +11,32 @@
 <script>
     export default {
         props:['ctx'],
+        mounted(){
+            if(this.ctx.css){
+                ex.append_css(this.ctx.css)
+            }
+        },
+        methods:{
+            get_style(image_url){
+                return {'background-image':'url('+image_url +')'}
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
+    .img-container{
+        background-size: cover;
+        background-position: center;
+        width: 100vw;
+        height: 5rem;
+    }
+    /*img{*/
+        /*width: 100vw;*/
+        /*height: auto;*/
+    /*}*/
     .my-swipe .van-swipe-item {
-        img{
-            max-width: 100vw;
-            height: auto;
-        }
+
         /*color: #fff;*/
         /*font-size: 20px;*/
         /*line-height: 150px;*/
