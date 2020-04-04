464d24903498db2acdc182291dd6c4c3eb3494d1
he_yulin <he_yulin@163.com>
2020-04-04 22:39:30 +0800

    debug

--- pcweb/js/top/main.js
+++ pcweb/js/top/main.js
@@ -3,4 +3,8 @@ import *  as  swiper_fade from './swiper_fade'
 import *  as  block_ctn from './block_ctn'
 import *  as  transparent_ctn from './transparent_ctn'
 import *  as  lay_main_small from './lay_main_small'
-import *  as  my_html from './html'
+//import *  as  my_html from './html'
+import  com_html from  './html.vue'
+import com_list from './list.vue'
+
+Vue.component('com-top-list',com_list)
