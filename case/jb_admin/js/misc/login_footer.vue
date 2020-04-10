179d98f6aea55709b5422029bb4d5699f8e9e04b
he_yulin <he_yulin@163.com>
2020-04-10 11:11:32 +0800

    改进登录界面

---
+++ case/jb_admin/js/misc/login_footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="com-ui-login-footer" >
+        <span v-text="ctx.copyright"></span>
+    </div>
+</template>
+<script>
+export default {
+    props:['ctx']
+}
+</script>
+<style lang="scss" scoped>
+.com-ui-login-footer{
+    text-align: center;
+    position: fixed;
+    bottom: 30px;
+    width: 100%;
+    /*position: absolute;*/
+    /*bottom: -130px;*/
+    /*width: 30em;*/
+}
+</style>
