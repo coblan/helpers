e2fffca612e9d23fa9e6fe1bd09f29d0097e780e
he_yulin <he_yulin@163.com>
2020-06-04 10:43:37 +0800

    dd

--- case/jb_admin/js/field_editor/main.js
+++ case/jb_admin/js/field_editor/main.js
@@ -3,7 +3,7 @@ import * as line_text from  './line_text'
 
 import * as phone_code from  './phone_code'
 import * as month from  './month'
-import * as radio from  './radio'
+
 import * as blocktext from  './blocktext'
 import * as cascader from  './cascader'
 import * as field_date from  './date'
@@ -24,8 +24,10 @@ import int_field from './int.vue'
 import  password from  './password.vue'
 import  select from  './select.vue'
 import field_bool from  './bool.vue'
+import  radio from  './radio.vue'
 
 Vue.component('com-field-int',int_field)
 Vue.component('com-field-password',password)
 Vue.component('com-field-select',select)
 Vue.component('com-field-bool',field_bool)
+Vue.component('com-field-radio',radio)
