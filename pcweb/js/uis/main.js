af30c6fdf6945461e9ba38246907f254622dca34
he_yulin <he_yulin@163.com>
2020-06-19 09:24:49 +0800

    dd

--- pcweb/js/uis/main.js
+++ pcweb/js/uis/main.js
@@ -1,3 +1,7 @@
 import  com_blank from  './blank.vue'
-
 Vue.component('com-ui-blank',com_blank)
+
+import  block_title_plain from './block_title/plain.vue'
+import  block_title_line from './block_title/title_line.vue'
+Vue.component('com-block-title-plain',block_title_plain)
+Vue.component('com-block-title-line',block_title_line)
