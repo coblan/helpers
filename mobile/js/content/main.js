6a4c6660e9f2a6fe7fb7c2dfe388bf6b4d0c2d49
he_yulin <he_yulin@163.com>
2020-06-28 00:27:27 +0800

    dd

---
+++ mobile/js/content/main.js
@@ -0,0 +1,6 @@
+import  title_block from './title_block.vue'
+import slot_wrap from './slot_wrap.vue'
+import slot_html from './slot_html.vue'
+
+Vue.component('com-title-block',title_block)
+Vue.component('com-slot-wrap',slot_wrap)
---
+++ mobile/js/content/title_block.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="com-title-block" :class="ctx.class">
+        <slot name="title">
+            <div class="title" v-text="ctx.title"></div>
+            <div class="sub-title" v-text="ctx.sub_title"></div>
+        </slot>
+        <slot name="content">
+            <div class="normal" v-html="ctx.html"></div>
+        </slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['ctx'],
+        data(){
+            var childStore = new Vue()
+            childStore.vc=this
+            return {
+                childStore:childStore
+            }
+        },
+        mounted(){
+            if(this.ctx.css){
+                ex.append_css(this.ctx.css)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .title{
+        text-align: center;
+        font-size: .4rem;
+        font-weight: bold;
+        margin: .3rem;
+    }
+    .sub-title{
+        text-align: center;
+        color: grey;
+        font-size: .26rem;
+    }
+.normal{
+    padding: .3rem;
+    line-height: .4rem;
+}
+</style>
