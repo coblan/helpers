33e3b98298192073453bad2ab5aea54fc01599b2
he_yulin <he_yulin@163.com>
2020-08-10 18:39:31 +0800

    dd

--- case/jb_admin/js/field_editor/blocktext.js
+++ case/jb_admin/js/field_editor/blocktext.js
@@ -1,7 +1,7 @@
 Vue.component('com-field-blocktext',{
         props:['row','head'],
         template: `<div>
-            <span v-if='head.readonly' v-text='row[head.name]'></span>
+            <pre v-if='head.readonly' v-text='row[head.name]'></pre>
             <textarea :style="head.style" v-else :maxlength="head.maxlength" class="form-control input-sm"
                 :name="head.name"
                 :id="'id_'+head.name" v-model="row[head.name]" :placeholder="head.placeholder"
