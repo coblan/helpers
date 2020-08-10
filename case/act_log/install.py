8b6314ecb69979198b3fa31f0db9e3d621a6755b
he_yulin <he_yulin@163.com>
2020-08-10 18:26:32 +0800

    改进

---
+++ case/act_log/install.py
@@ -0,0 +1,91 @@
+
+import os
+import sys
+def SET(scope):
+    BASE_DIR= scope['BASE_DIR']
+    LOG_PATH= os.path.join( os.path.dirname(BASE_DIR),'log')
+
+    LOGGING = {
+      'version': 1, # 标示配置模板版本，int 类型，目前只接收 `1`这个值。
+      'disable_existing_loggers': False, 
+      'formatters': {
+          'standard': {
+               #'format': '%(levelname)s %(asctime)s %(message)s',
+              'format': '%(levelname)s %(asctime)s %(process)d-%(thread)d %(message)s'
+          },
+      },
+      'filters': {
+          # 这里是定义过滤器，需要注意的是，由于 'filters' 是 logging.config.dictConfig 方法要求在配置字典中必须给订的 key ,所以即使不使用过滤器也需要明确给出一个空的结构。
+      },
+      'handlers': {
+           'mail_admins': {
+              'level': 'ERROR',
+              'class': 'django.utils.log.AdminEmailHandler',
+              'formatter':'standard',
+          },
+  
+          'console': {
+              'level':'DEBUG',
+              'class': 'logging.StreamHandler',
+              'stream': sys.stdout
+              }, 
+          
+          'error_warning':{
+              'level': 'WARNING',
+              'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+              'maxBytes': 1024*1024*5,
+              'backupCount':3,
+              'formatter':'standard',
+              'filename': os.path.join(LOG_PATH,'errors.log'),    
+              'encoding': 'utf8',
+              
+              },
+          'general_log':{
+              'level': 'DEBUG',
+              #'class': 'logging.handlers.RotatingFileHandler',
+              'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+              'maxBytes': 1024*1024*5,
+              'backupCount':3,
+              'formatter':'standard',
+              'filename': os.path.join(LOG_PATH,'general_log.log'),     
+              'encoding': 'utf8',
+            }, 
+          'backend_operation':{
+              'level': 'DEBUG',
+              #'class': 'logging.handlers.RotatingFileHandler',
+              'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+              'maxBytes': 1024*1024*5,
+              'backupCount':3,
+              'formatter':'standard',
+              'filename': os.path.join(LOG_PATH,'backend_operation.log'),     
+              'encoding': 'utf8',
+            },            
+      },
+      'loggers': {
+          '': {
+              'handlers': ['mail_admins', 'error_warning'], # 'elk_warning',
+              'level': 'WARNING',
+              'propagate': False,
+          },
+          'django': {
+              'handlers': ['console', ] , #'djangoout_warning', 'mail_admins', 'elk_warning'],
+              'level': 'INFO',
+              'propagate': True,
+              },          
+  
+          'general_log': {
+              'handlers': ['console', 'general_log', ] ,# 'elk_info' ],  #'djangoout_warning',
+              'level': 'DEBUG',
+              'propagate': False,            
+              },
+          'ModelFields.save_form': {
+              'handlers': ['backend_operation'] , # 'elk_debug'],
+              'level': 'DEBUG',
+              'propagate': False,              
+              },
+      }
+    }
+    
+    scope.update({
+        'LOGGING':LOGGING,
+    })
--- case/act_log/models.py
+++ case/act_log/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class GeneralLog(models.Model):  
+    createtime = models.DateTimeField(verbose_name='创建时间',)
+    level = models.CharField('等级',max_length=20,blank=True)
+    message =  models.TextField(verbose_name='消息体',blank=True)
+    path = models.CharField('消息来源',max_length=300,blank=True)
+    process = models.CharField('进程ID',max_length=100,blank=True)
+    host = models.CharField('主机名',max_length=200,blank=True)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['createtime']),
+        ]
