3fa4d4a42b6f061dff96bedd426e5cc3c1ebb2a2
he_yulin <he_yulin@163.com>
2020-08-28 00:46:38 +0800

    引入 transfer控件

--- case/jb_admin/js/field_editor/ele_transfer.js
+++ case/jb_admin/js/field_editor/ele_transfer.js
@@ -1,16 +1,30 @@
 var ele_transfer={
     props:['row','head'],
-    template:`<div>
-     <el-transfer v-model="value1" :data="trans_data"></el-transfer>
+    template:`<div class="com-field-ele-transfer" :class="head.class">
+     <el-transfer v-model="row[head.name]" :data="head.options"
+     :props="{
+      key: 'value',
+    }"
+     :filterable="head.searchable"
+    :filter-method="filterMethod"
+     :titles="['可选项','已选项']"></el-transfer>
      </div>`,
     data:function(){
         return {
             value1:[]
         }
     },
+    mounted(){
+        if(this.head.css){
+            ex.append_css(this.head.css)
+        }
+    },
     computed:{
-        trans_data:function(){
-            return [{key:'xx',label:'bbbb'}]
+        filterMethod(query, item){
+            if(item){
+                return item.label.index(query) > -1
+            }
+
         }
     }
 }
