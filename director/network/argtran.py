1f2c9a96ba65c74a815df98a81c3cbbf9f38cbd7
he_yulin <he_yulin@163.com>
2020-08-31 22:34:34 +0800

    dd

--- director/network/argtran.py
+++ director/network/argtran.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 
 def adapt_date_splash(value,name):
     "未完成！！！ 将 2020/6/1  转换为 2020-06-01"
@@ -6,3 +7,12 @@ def adapt_date_splash(value,name):
         return '-'.join(ls)
     else:
         return value
+
+def decimal_round(digits=2):
+    def _fun(value,name):
+        if value:
+            aa = Decimal(value)
+            return round(aa,digits)
+        else:
+            return value
+    return _fun
