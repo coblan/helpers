da365e4e971fe5900b90995de0e5741b856a80d9
he_yulin <he_yulin@163.com>
2020-09-26 12:41:26 +0800

    dd

---
+++ case/jb_admin/js/misc/el_drop_btn.vue
@@ -0,0 +1,114 @@
+<template>
+    <span class="com-drop-btn" :class="my_ctx.class">
+        <!--<el-button size="mini" :type="my_ctx.type" @click="on_click()"-->
+                   <!--:title="ctx.title"-->
+                   <!--:disabled="is_disabled" :plain="my_ctx.plain">-->
+            <!--<slot name="content">-->
+                <!--<i v-if="my_ctx.icon" :class="my_ctx.icon"></i>-->
+                <!--<span v-text="my_ctx.label"></span>-->
+            <!--</slot>-->
+        <!--</el-button>-->
+
+        <!--split-button :type="my_ctx.type"-->
+        <el-dropdown >
+            <el-button  size="mini" :disabled="is_disabled">
+               <span class="el-dropdown-link">
+                 <span v-text="my_ctx.label"></span>
+                  <i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+            </el-button>
+            <el-dropdown-menu slot="dropdown" >
+                <el-dropdown-item v-for="action in my_ctx.menu" :disabled="action_disable(action)">
+                    <span :title="action.title" @click="on_click_action(action)" v-text="action.label"></span>
+                </el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+    </span>
+</template>
+<script>
+    export default {
+        props:['ctx','head'],
+        data(){
+            return {
+                parStore:ex.vueParStore(this),
+                // TODO 剔除某些老组件后，需要移除 head的引用
+                my_ctx:this.ctx?this.ctx:this.head,
+            }
+        },
+        computed:{
+            is_disabled(){
+                if(this.my_ctx .disabled_express){
+                    return  ex.eval(this.my_ctx.disabled_express,{ps:this.parStore})
+                }else{
+                    return false
+                }
+            }
+        },
+//        watch:{
+//            is_disabled(nv){
+//                if(nv){
+//                    this.set_btn_group_disable()
+//                }else{
+//                    this.clear_btn_group_disable()
+//                }
+//            }
+//        },
+        mounted(){
+            if(this.ctx.css){
+                ex.append_css(this.ctx.css)
+            }
+//            if(this.is_disabled){
+//                this.set_btn_group_disable()
+//            }
+        },
+        methods:{
+            set_btn_group_disable(){
+                $(this.$el).find('.el-button').addClass('is-disabled')
+                $(this.$el).find('.el-button').prop('disabled',true)
+            },
+            clear_btn_group_disable(){
+                $(this.$el).find('.el-button').removeClass('is-disabled')
+                $(this.$el).find('.el-button').prop('disabled',false)
+            },
+//            set_drop_disable(value ){
+//                if(value){
+//                    $(this.$el).find('.el-dropdown__caret-button').addClass('is-disabled')
+//                    $(this.$el).find('.el-dropdown__caret-button').prop('disabled',true)
+//                }else{
+//                    $(this.$el).find('.el-dropdown__caret-button').removeClass('is-disabled')
+//                    $(this.$el).find('.el-dropdown__caret-button').prop('disabled',false)
+//                }
+//            },
+            action_disable(action){
+                if(action.disabled_express){
+                    return ex.eval(action.disabled_express,{ps:this.parStore})
+                }else{
+                    return false
+                }
+            },
+//            on_group_click(){
+//                this.on_click_action(this.my_ctx)
+//            },
+            on_click_action(action){
+                if(action.click_express){
+
+                    // 在table组件中，会先检查选中的row。
+                    if(action.row_match){
+                        var p = this.parStore.check_selected(action)
+                    }else{
+                        var p = Promise.resolve()
+                    }
+                    p.then(()=>{
+                        ex.eval(action.click_express,{head:action,ps:this.parStore,vc:this})
+                })
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .com-btn{
+        margin: 0 1px;
+    }
+</style>
