76ba4fc2534251c5f776c30d0f40cb4cc6954915
hyl <coblan@163.com>
2020-11-25 17:55:28 +0800

    dd

--- case/jb_admin/js_cfg.py
+++ case/jb_admin/js_cfg.py
@@ -111,9 +111,12 @@ def get_lib(request):
             'adminlte_css': 'https://cdn.jsdelivr.net/npm/admin-lte@2.4.10/dist/css/AdminLTE.min.css',
             'adminlte_blue_css': 'https://cdn.jsdelivr.net/npm/admin-lte@2.4.10/dist/css/skins/skin-blue.min.css',            
             'nprogress': 'https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.min.js',
-            'nprogress_css': 'https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.css',        
-            'element': 'https://cdn.jsdelivr.net/npm/element-ui@2.10.0/lib/index.js',
-            'element_css':'https://cdn.jsdelivr.net/npm/element-ui@2.10.0/lib/theme-chalk/index.css',  #2.8.2
+            'nprogress_css': 'https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.css',   
+            
+            'element': 'https://cdn.jsdelivr.net/npm/element-ui@2.14.1/lib/index.js', #2.10.0
+            'element_css':'https://cdn.jsdelivr.net/npm/element-ui@2.14.1/lib/theme-chalk/index.css',  #2.8.2
+   
+   
             'md5': 'https://cdn.jsdelivr.net/npm/blueimp-md5@2.10.0/js/md5.min.js',
             'swiper': 'https://cdn.jsdelivr.net/npm/swiper@4.5.0/dist/js/swiper.min.js',
             'swiper_css': 'https://cdn.jsdelivr.net/npm/swiper@4.5.0/dist/css/swiper.min.css',  
