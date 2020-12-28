72ab6287d38e918d058910ed54c26367d34f888a
hyl <coblan@163.com>
2020-12-28 12:34:43 +0800

    dd

--- director/js/exfun/obj.js
+++ director/js/exfun/obj.js
@@ -41,7 +41,7 @@ export var obj_control={
         //}
     },
     update_row(row){
-        return ex.diretor_call('d.get_row',{pk:row.pk,director_name:row._director_name}).then((rt_row)=>{
+        return ex.director_call('d.get_row',{pk:row.pk,director_name:row._director_name}).then((rt_row)=>{
             ex.vueAssign(row,rt_row)
         })
     }
