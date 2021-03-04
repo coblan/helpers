e4970cb469f3572646aa24e161c4ec63f37ec49b
hyl <coblan@163.com>
2021-03-04 11:47:58 +0800

    dd

--- maintenance/db/copy.py
+++ maintenance/db/copy.py
@@ -3,7 +3,13 @@ from helpers.director.model_func.dictfy import sim_dict
 def table_rows(src_query,model):
     ls = []
     for inst in src_query:
-        dc = sim_dict(inst,include_pk=False)
+        dc = inst.__dict__ #sim_dict(inst,include_pk=False)
+        rm_keys=[]
+        for k in dc:
+            if k.startswith('_'):
+                rm_keys.append(k) 
+        for key in rm_keys:
+            dc.pop(key)
         ls.append(model(**dc))
         if len(ls) > 2000:
             yield ls
