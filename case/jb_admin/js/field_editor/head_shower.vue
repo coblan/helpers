ef42ecfcb0787078ca2dac069faf19fbd2499af7
hyl <coblan@163.com>
2021-03-05 15:37:17 +0800

    dd

---
+++ case/jb_admin/js/field_editor/head_shower.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="com-field-head-shower">
+        <div class="readonly-info" v-html='head.html'></div>
+    </div>
+</template>
+<script>
+    export default {
+        props:['row','head']
+    }
+</script>
+<style scoped lang="scss">
+.com-field-head-shower{
+    font-size: 12px;
+    color: #797979;
+}
+</style>
