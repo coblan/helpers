b37ae50cc5adf608bd12d9a0a764827fd109ba93
hyl <coblan@163.com>
2021-06-30 21:09:24 +0800

    vf

--- func/collection/mylist.py
+++ func/collection/mylist.py
@@ -3,3 +3,11 @@ def split_list(dstlist,every_num):
     while len(dstlist) > count*every_num:
         yield dstlist[every_num*count:every_num*(count+1)]
         count += 1
+
+def left_join(left,right,func):
+    for row in left:
+        for ds in right:
+            if func(row,ds):
+                row.update(ds)
+                right.remove(ds)
+                break
