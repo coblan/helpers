8167667e08d4602a54d9625ab5b9cdc3b54e4921
coblan <coblan@163.com>
2021-08-19 23:35:59 +0800

    debug

--- case/jb_admin/js/pannels/backendTable.vue
+++ case/jb_admin/js/pannels/backendTable.vue
@@ -13,8 +13,8 @@
             :parents="ctx.parents"
             :search-args="ctx.search_args"
             ref="dtable"
-            :extend-logic="extendLogic"
     >
+      <!--            :extend-logic="extendLogic"-->
 
     </director_table>
 </template>
@@ -22,39 +22,39 @@
     import  director_table,{DTableLogic} from  './director_table.vue'
     const { ref, reactive,computed ,onMounted,getCurrentInstance } = VueCompositionAPI
 
-    class DTable2Logic extends DTableLogic{
-        getSetup(props){
-            const vc = getCurrentInstance()
-            onMounted(()=>{
-                if(vc.proxy.tableRows.length==0){
-                    vc.proxy.search()
-                }
-            })
-            return {
+    // class DTable2Logic extends DTableLogic{
+    //     getSetup(props){
+    //         // const vc = getCurrentInstance()
+    //         // onMounted(()=>{
+    //         //     if(vc.proxy.tableRows.length==0){
+    //         //         vc.proxy.search()
+    //         //     }
+    //         // })
+    //         return {
+    //
+    //         }
+    //     }
+    // }
 
-            }
-        }
-    }
-
-    class BackendTable{
-        getSetup(props){
-            this. dtable = ref(null)
-            this.connect_dtable_event()
-            return {
-                dtable:this.dtable,
-//                extendLogic:DTable2Logic,
-            }
-        }
-        connect_dtable_event(){
-            const vc = getCurrentInstance()
-            onMounted(()=>{
-                this.dtable.value.childStore.$on('finish',(data)=>{
-                    vc.emit('finish',data)
-                })
-            })
-        }
-
-    }
+//     class BackendTable{
+//         getSetup(props){
+//             this. dtable = ref(null)
+//             this.connect_dtable_event()
+//             return {
+//                 dtable:this.dtable,
+// //                extendLogic:DTable2Logic,
+//             }
+//         }
+//         connect_dtable_event(){
+//             const vc = getCurrentInstance()
+//             onMounted(()=>{
+//                 this.dtable.value.childStore.$on('finish',(data)=>{
+//                     vc.emit('finish',data)
+//                 })
+//             })
+//         }
+//
+//     }
     export default {
         components:{
             director_table,
@@ -62,12 +62,21 @@
         props:['ctx'],
         data(){
             return {
-                extendLogic:DTable2Logic
+
+                // extendLogic:DTable2Logic
             }
         },
-        setup(props){
-            debugger
-            return new BackendTable().getSetup(props)
+        // setup(props){
+        //     debugger
+        //     return new BackendTable().getSetup(props)
+        // },
+        mounted(){
+          if(!this.ctx.rows || this.ctx.rows.length==0){
+              this.$refs.dtable.search()
+          }
+          this.$refs.dtable.childStore.$on('finish',(data)=>{
+              this.emit('finish',data)
+          })
         }
     }
 </script>
