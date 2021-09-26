22c9cce56e458bcb91887e5dfd7a9d1ed5852ac3
coblan <coblan@163.com>
2021-09-27 00:57:45 +0800

    auto commit

--- func/d_import.py
+++ func/d_import.py
@@ -2,6 +2,11 @@ import importlib
 import re
 
 def import_element(element_path):
+    """以字符串的形式导入模块类的函数或者类
+    
+    Args:
+    element_path:a.b.c;  .c 是类或者函数
+    """
     ls = element_path.split('.')
     pkg_path = '.'.join(ls[:-1])
     pkg = importlib.import_module(pkg_path)
