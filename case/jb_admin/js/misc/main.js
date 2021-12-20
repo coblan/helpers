dd96a7012413ad4398a70b7218c804818a8683f5
coblan <coblan@163.com>
2021-12-21 01:43:07 +0800

    auto commit

--- case/jb_admin/js/misc/main.js
+++ case/jb_admin/js/misc/main.js
@@ -6,7 +6,7 @@ import el_drop_btn from  './el_drop_btn.vue'
 import  vertical_layout from  './vertical_layout.vue'
 import  el_tab from './el_tab_widget.vue'
 import  line_info from  './line_info.vue'
-
+import messageInfo from "./messageInfo.vue";
 Vue.component('com-ui-login-head',login_head)
 Vue.component('com-ui-login-footer',login_footer)
 Vue.component('com-btn',el_button)
@@ -15,4 +15,5 @@ Vue.component('com-layout-vertical',vertical_layout)
 Vue.component('com-line-info',line_info)
 
 Vue.component('com-widget-el-tab',el_tab)
+Vue.component('com-message-rows',messageInfo)
 window.live_el_tab = el_tab
---
+++ case/jb_admin/js/misc/messageInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="com-message-rows">
+    <template  v-for='(row,index) in ctx.messages'>
+      <span style="display: inline-block;padding: 0 10px">{{index+1}}:</span>
+      <template v-if="row.type">
+          <span :class="row.type">{{row.message}}</span>
+      </template>
+      <span v-else>{{row}}</span>
+    </template>
+  </div>
+</template>
+<script>
+/*
+* ctx: {messages}
+* */
+export default {
+  props:{
+    ctx:{}
+  }
+}
+</script>
+<style scoped lang="scss">
+.com-message-rows{
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
+.error{
+  color: red;
+}
+</style>
