2f82ac5aa7ddb4d37afcd1cd001c94f4f206888c
coblanplus <coblanplus@gmail.com>
2022-01-24 18:40:14 +0800

    dd

--- case/jb_admin/js/header/historyPage.vue
+++ case/jb_admin/js/header/historyPage.vue
@@ -11,8 +11,9 @@
 <!--           @close="handleClose(tag)">-->
 <!--         {{page.label}}-->
 <!--       </el-tag>-->
-       <div class="wrap" v-for="page in pagelist">
-         <div class="page-label clickable"  @click="goto(page)">{{page.label}}</div>
+       <i class="fa fa-history toggle-button" :class="{active:show_history}" @click="show_history = !show_history" aria-hidden="true"></i>
+       <div class="wrap" v-for="page in pagelist" v-if="show_history">
+         <div class="page-label clickable" :class="{active:page.name==current_name}"  @click="goto(page)">{{page.label}}</div>
          
 <!--         <i class="fa fa-lock" aria-hidden="true"></i>-->
        </div>
@@ -25,7 +26,9 @@
 export default {
   data(){
     return {
-        pagelist:[]
+        show_history:true,
+        pagelist:[],
+        current_name:page_name,
     }
   },
   mounted(){
@@ -44,11 +47,16 @@ export default {
       }
       var one = ex.findone(pp,{name:page_name})
       if(one){
-        var index = pp.indexOf(one)
-        pp.splice(index,1)
-        pp.splice(0,0,one)
+        // var index = pp.indexOf(one)
+        // pp.splice(index,1)
+        // pp.splice(0,0,one)
       }else{
-        pp.splice(0,0, {href:location.href,label:page_label,name:page_name})
+        if(page_name){
+          pp.splice(0,0, {href:location.href,label:page_label,name:page_name})
+        }
+      }
+      if(pp.length>5){
+        pp.pop()
       }
       this.pagelist  = pp
       localStorage.setItem('_history-page',JSON.stringify(this.pagelist))
@@ -62,6 +70,8 @@ export default {
 <style scoped lang="scss">
 .history-page{
   display:  inline-block;
+
+  padding: 15px 0;
 }
 .wrap{
   display: inline-block;
@@ -69,9 +79,15 @@ export default {
 }
 .page-label{
     display: inline-block;
-
-    color: #f1f1f1;
-    padding: 15px 0;
+    color: #b4abab;
 }
-
+.toggle-button{
+  color:grey;
+  &.active{
+    color: white;
+  }
+}
+.active{
+  color: white;
+}
 </style>
