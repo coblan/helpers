bc0b5c89ef37fc01fbe73bbd7a02697b885f8dbd
coblan <coblan@163.com>
2022-04-28 20:59:02 +0800

    dd

--- authuser/js/main.js
+++ authuser/js/main.js
@@ -1,6 +1,8 @@
 import * as com_login_banner from  './com_login_banner.js'
 import * as fields_editor_auth_validate_code from  './fields_editor/auth_validate_code.js'
 require('weblib/pc/style/element_ex.scss')
+import 'weblib/style'
+
 import loginPage from "./loginPage.vue";
 Vue.component('login-form-jb',loginPage)
 
