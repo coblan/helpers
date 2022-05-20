88ab30eee8f7f85d13c63c836ee6b4770c7ab8d9
coblanplus <coblanplus@gmail.com>
2022-05-20 18:32:37 +0800

    增加head

---
+++ case/jb_admin/js/header/datetime.vue
@@ -0,0 +1,60 @@
+<template>
+  <li class="datetime-pannel">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+        <span>星期{{week}}</span>
+        <span style="display: inline-block;margin: 0 3px;">{{date}}</span>
+        <i class="fa fa-clock-o" aria-hidden="true"></i>
+        <span>{{time}}:{{pad(second)}}</span>
+    </a>
+  </li>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      date:'',
+      week:'',
+      time:'',
+      second:0,
+    }
+  },
+  mounted(){
+    this.update()
+    setInterval(()=>{
+      this.updateSecond()
+    },1000)
+
+  },
+
+  methods:{
+    pad(number){
+      return `${number}`.padStart(2,0)
+    },
+      update(){
+        var now = ex.dayjs()
+        this.date = now.format('YYYY/MM/DD')
+        this.week = now.format('d')
+        if(now.hour() >12){
+          this.time = '下午'+ now.format('HH:mm')
+        }else{
+          this.time = '上午'+ now.format('HH:mm')
+        }
+        this.second = now.second()
+      },
+      updateSecond(){
+        if(this.second ==59){
+          this.update()
+        }else{
+          this.second +=1
+        }
+      }
+  },
+
+}
+</script>
+<style scoped lang="scss">
+.datetime-pannel{
+  float: left;
+}
+</style>
