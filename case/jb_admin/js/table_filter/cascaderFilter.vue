97ee3bb31225e7df86688e54db04ad92fd8bd731
coblan <coblan@163.com>
2022-05-20 23:25:17 +0800

    dd

---
+++ case/jb_admin/js/table_filter/cascaderFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="com-filter-cascasder">
+    <el-cascader
+        :show-all-levels="false"
+        v-model="search_args[head.name]"
+        :options="head.options"
+        :props="{checkStrictly: true ,emitPath:false}"
+        @change="onChange"
+        size="small"
+        clearable>
+    </el-cascader>
+  </div>
+</template>
+<script>
+export  default  {
+  props:['head','search_args'],
+  methods:{
+    onChange(){
+      if(!this.search_args[this.head.name]){
+        delete this.search_args[this.head.name]
+      }
+    }
+  }
+}
+</script>
+
--- case/jb_admin/js/table_filter/main.js
+++ case/jb_admin/js/table_filter/main.js
@@ -1,6 +1,9 @@
-import * as cascader_filter from './cascader_filter'
+
 
 import * as multi_select from './multi_select'
 import exact_text from  './exact_text.vue'
 
 Vue.component('com-filter-exact-text',exact_text)
+
+import cascaderFilter from "./cascaderFilter.vue";
+Vue.component('com-filter-cascasder',cascaderFilter)
