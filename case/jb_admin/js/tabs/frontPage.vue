6907f9caa57e9247c60df007a7db76533d71e08a
coblan <coblan@163.com>
2022-07-15 02:14:42 +0800

    dd

---
+++ case/jb_admin/js/tabs/frontPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="myfront">
+<!--    <com-form-one :ctx="$attrs.tab_head.fields_ctx"></com-form-one>-->
+    <compnent :is="$attrs.tab_head.inn_editor_list[index]" v-bind="$attrs" @finish="onFinish"></compnent>
+<!--    <com-tab-form v-bind="$attrs"></com-tab-form>-->
+  </div>
+
+</template>
+<script>
+export default {
+  data(){
+    return {
+      index:0,
+    }
+  },
+
+  methods:{
+    onFinish(){
+      this.index+=1
+      console.log('finish')
+      // if(this.$attrs.next_express){
+      //   ex.eval(this.$attrs.next_express,{vc:this})
+      // }else{
+      //   this.index+=1
+      // }
+
+    }
+  }
+}
+</script>
