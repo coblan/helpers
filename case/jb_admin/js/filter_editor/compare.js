78d5796fc2a165fea7516e456522b45adbef84d9
coblan <coblan@163.com>
2022-09-09 13:28:46 +0800

    dd

--- case/jb_admin/js/filter_editor/compare.js
+++ case/jb_admin/js/filter_editor/compare.js
@@ -4,6 +4,8 @@ export default  {
     data:function(){
         this.search_args['_'+this.head.name+'_compare'] =  this.search_args['_'+this.head.name+'_compare'] || '0'
         return {
+            // comparetype:this.search_args['_'+this.head.name+'_compare'] || '0',
+            inn_value :this.search_args[this.head.name] || ''
         }
     },
     template:`<div  class="com-filter-datetime-range flex flex-ac" :style="{width:head.width}">
@@ -13,8 +15,32 @@ export default  {
                          <option value="1">≥</option>
                          <option value="-1">≤</option>
                    </select>
-                   <input @keyup.enter="parStore.search()" type="text" v-model='search_args[head.name]' class="form-control input-sm" :placeholder="head.label">
+                   <input @keyup.enter="parStore.search()" type="text" v-model='inn_value' class="form-control input-sm" :placeholder="head.label">
                 </div>`,
+    watch:{
+        inn_value(nv){
+            // var compare_name = '_'+this.head.name+'_compare'
+            if(nv){
+                this.$set(this.search_args,this.head.name,nv)
+                // this.$set(this.search_args,compare_name,this.comparetype)
+            }else{
+                Vue.delete(this.search_args,this.head.name)
+                // Vue.delete(this.search_args,compare_name)
+            }
+        },
+        out_value(nv){
+            if(nv){
+                this.inn_value = nv
+            }else{
+                this.inn_value = ''
+            }
+        }
+    },
+    computed:{
+        out_value(){
+            return this.search_args[this.head.name]
+        }
+    }
 
 
 
