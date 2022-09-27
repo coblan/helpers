d0cad95accb783f964d870daa889a54c50c3e7e2
coblanplus <coblanplus@gmail.com>
2022-09-27 18:24:55 +0800

    增加

--- func/rabbit_instance.py
+++ func/rabbit_instance.py
@@ -2,6 +2,9 @@ from django.conf import settings
 import pika
 
 def get_rabbit_instance():
+    """
+    现在使用celery的客户端发送消息，暂时不使用pika了
+    """
     credentials = pika.PlainCredentials(settings.RABBIT_USER,settings.RABBIT_PSWD)
     connection =pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBIT_SERVER,credentials=credentials))
     channel = connection.channel()
