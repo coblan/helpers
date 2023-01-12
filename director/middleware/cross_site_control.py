a171b4d510770f16700f0c401e64daec088306d3
coblanplus <coblanplus@gmail.com>
2023-01-12 17:45:37 +0800

    跨域

--- director/middleware/cross_site_control.py
+++ director/middleware/cross_site_control.py
@@ -3,7 +3,11 @@ from django.utils.deprecation import MiddlewareMixin
 
 class CrossSiteControl(MiddlewareMixin):
     def process_response(self,request,response):
-        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Origin'] = request.META.HTTP_HOST
+        response['Access-Control-Allow-Headers'] = 'X-Requested-With'
+        response['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
+        response['Access-Control-Allow-Credentials'] = 'true'
+
         if request.method == 'OPTIONS':
             response['Access-Control-Allow-Headers'] = 'Content-Type'
         return response
