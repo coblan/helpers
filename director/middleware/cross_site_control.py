9d50fecab6975cb7b123e433e558bb7d221b575b
coblanplus <coblanplus@gmail.com>
2023-01-12 17:50:02 +0800

    gwe

--- director/middleware/cross_site_control.py
+++ director/middleware/cross_site_control.py
@@ -3,7 +3,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 class CrossSiteControl(MiddlewareMixin):
     def process_response(self,request,response):
-        response['Access-Control-Allow-Origin'] = request.META.HTTP_HOST
+        response['Access-Control-Allow-Origin'] = request.META.get('HTTP_HOST')
         response['Access-Control-Allow-Headers'] = 'X-Requested-With'
         response['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
         response['Access-Control-Allow-Credentials'] = 'true'
