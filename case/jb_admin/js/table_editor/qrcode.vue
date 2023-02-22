45cbf0c9b6b4a91a128471ee55b5eef850083bd0
coblanplus <coblanplus@gmail.com>
2023-02-22 15:25:36 +0800

    gjkl

--- case/jb_admin/js/table_editor/qrcode.vue
+++ case/jb_admin/js/table_editor/qrcode.vue
@@ -1,9 +1,35 @@
 <template>
-  <div style="padding: 10px">
-    <div class="qr-pad"></div>
+  <div style="padding: 10px" @click="open_qr">
+    <div class="qr-pad clickable"></div>
   </div>
 </template>
 <script>
+
+var qrcode_show = {
+  template:`<div style="padding: 10px;background-color: white;position: absolute;left: 50%;top:50%;transform: translate(-50%,-50%)">
+      <div class="qr-pad"></div>
+  </div>`,
+  props:{
+    text:{}
+  },
+  mounted(){
+    this.genQrcode()
+  },
+  methods:{
+    genQrcode(){
+      var dom = this.$el.querySelector('.qr-pad')
+      var qrcode = new QRCode(dom, {
+        text: this.text,
+        width: 324,
+        height: 324,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+      });
+    },
+  }
+}
+
 export  default {
   props:['rowData','field','index'],
   async mounted(){
@@ -21,6 +47,15 @@ export  default {
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
       });
+    },
+    open_qr(){
+      cfg.pop_vue_com(qrcode_show,{text:this.rowData[this.field]},{
+        title:false,
+        area: ['90%', '90%'],
+        shade: 0.8,
+        skin: 'img-shower',
+        shadeClose: true,
+      })
     }
   }
 }
