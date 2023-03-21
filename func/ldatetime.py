b20559a6a20b61b75d033695624b667fbf10d573
coblanplus <coblanplus@gmail.com>
2023-03-21 15:48:50 +0800

    gjk

--- func/ldatetime.py
+++ func/ldatetime.py
@@ -1,5 +1,14 @@
 
 from django.utils.timezone import localtime,datetime
+from dateutil.tz import *
+
+def to_utc(date_time):
+    if isinstance(date_time,str):
+        date_time_obj = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+    else:
+        date_time_obj = date_time
+    return date_time_obj.replace(tzinfo=tzlocal()).astimezone(tzoffset(None, 0))
 
 def localstr(date_obj):
     return localtime(date_obj).strftime('%Y-%m-%d %H:%M:%S')
+
