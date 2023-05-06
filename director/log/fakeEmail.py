bd08208754c8e2de49f74e28725debbd513c032e
coblanplus <coblanplus@gmail.com>
2023-05-06 18:02:05 +0800

    dg

--- director/log/fakeEmail.py
+++ director/log/fakeEmail.py
@@ -26,10 +26,11 @@ class MyAdminEmailHandler(AdminEmailHandler):
 
 def format_frames(frames):
     ls =[]
-    for row in frames:
+    
+    for row in  reversed( frames ):
         vars_list  = row.get('vars')
         vars_str =''
-        for v in reversed(vars_list):
+        for v in vars_list:
             value = unescape(v[1])
             vars_str += f'\t{v[0]}={value}\n'
         ls.append(str({
