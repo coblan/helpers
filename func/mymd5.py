bb631cf25cc2bf5259c258eeaceb40cb5495d7e2
coblanplus <coblanplus@gmail.com>
2023-05-09 20:37:09 +0800

    gs

--- func/mymd5.py
+++ func/mymd5.py
@@ -2,5 +2,8 @@ import hashlib
 
 def md5(data):
     md5 = hashlib.md5()
-    md5.update(data.encode('utf-8'))
+    if isinstance(data,str):
+        md5.update(data.encode('utf-8'))
+    else:
+        md5.update(data)
     return  md5.hexdigest()
