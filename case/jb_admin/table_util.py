e8ae834810dcc86b675f3ecb7144c3b22dd1ac09
coblanplus <coblanplus@gmail.com>
2023-05-22 17:23:26 +0800

    dgh

--- case/jb_admin/table_util.py
+++ case/jb_admin/table_util.py
@@ -1,9 +1,12 @@
 from django.utils import timezone
 
-def default_search_range_time(search_args,name, tm_delta):
+
+def default_search_range_time(search_args,name, tm_delta,second_only_promt = True):
     """
     如果没有日期，会输出默认日期
     """
+    if second_only_promt:
+        return check_search_range_time(search_args, name, tm_delta)
     start_str = search_args.get('_start_%s'%name)
     end_str =  search_args.get('_end_%s'%name)
     if start_str:
@@ -37,6 +40,49 @@ def default_search_range_time(search_args,name, tm_delta):
     if not end_str and end != now:
         search_args['_end_%s'%name] = end.strftime('%Y-%m-%d %H:%M:%S')
     return start,end
+
+def check_search_range_time(search_args,name, tm_delta):
+    start_str = search_args.get('_start_%s'%name)
+    end_str =  search_args.get('_end_%s'%name)
+    if start_str:
+        if isinstance(start_str,str):
+            start = timezone.datetime.strptime(start_str,'%Y-%m-%d %H:%M:%S')
+        else:
+            # 有可能在上游，start_str处理成了 datetime对象
+            start = start_str
+    else:
+        if '_not_first' in search_args:
+            raise UserWarning('必须输入开始时间')
+        start = None
+    if end_str:
+        if isinstance(end_str,str):
+            end = timezone.datetime.strptime(end_str,'%Y-%m-%d %H:%M:%S')
+        else:
+            end = end_str
+    else:
+        end = None
+        
+    now = timezone.now()  
+    if not start and not end:
+        start = now - tm_delta
+        end = now
+    if not end:
+        end = now
+    #elif start and not end:
+        #end = start + tm_delta
+    #elif not start and end:
+        #start = end - tm_delta
+    
+    if start > end:
+        raise UserWarning('起始时间不能大于结束时间')
+    if '_not_first' not in search_args:
+        search_args['_start_%s'%name] = start.strftime('%Y-%m-%d %H:%M:%S')
+    search_args['_not_first'] = 1
+    #if not end_str and end != now:
+        #search_args['_end_%s'%name] = end.strftime('%Y-%m-%d %H:%M:%S')
+    return start,end
+
+
 
 def default_search_range_date(search_args,name, tm_delta):
     """"""
