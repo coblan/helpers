c7fb39e4771b048bf8df6be0e035ea1d2e599792
coblanplus <coblanplus@gmail.com>
2023-07-14 18:23:53 +0800

    auto commit

--- func/myos.py
+++ func/myos.py
@@ -10,26 +10,30 @@ def is_install(soft_name):
     system_name = platform.system().lower()
     general_log.debug(system_name)
     
-    if system_name == 'windows':
-        print("system = windows")
-        ww = p.stdout.read()
-        general_log.debug(ww)
-        ww= ww.decode('gbk')
-        
-        if '不是内部或外部命令' in ww:
-            return False
-        else:
-            return True
-       
-    elif system_name == 'linux':
-        print('system = linux')
-        ww = p.stdout.read()
-        general_log.debug(ww)  
-        ww= ww.decode('utf-8') 
-        if 'not found' in ww:
-            return False
-        else:
-            return True
+    try:
+        if system_name == 'windows':
+            print("system = windows")
+            ww = p.stdout.read()
+            #general_log.debug(ww)
+            ww= ww.decode('gbk')
+            general_log.debug(ww)
+            if '不是内部或外部命令' in ww:
+                return False
+            else:
+                return True
+           
+        elif system_name == 'linux':
+            print('system = linux')
+            ww = p.stdout.read()
+            #general_log.debug(ww)  
+            ww= ww.decode('utf-8') 
+            general_log.debug(ww) 
+            if 'not found' in ww:
+                return False
+            else:
+                return True
+    except Exception as e:
+        general_log.exception(e)
         
         
        
