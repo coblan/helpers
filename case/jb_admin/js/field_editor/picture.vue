060889623c83c34aff8892d3d41607dc09e98ff6
coblanplus <coblanplus@gmail.com>
2023-07-20 15:17:35 +0800

    debug

--- case/jb_admin/js/field_editor/picture.vue
+++ case/jb_admin/js/field_editor/picture.vue
@@ -33,10 +33,14 @@ export default {
     }
   },
   mounted(){
+    if(this.head.mounted_express){
+      ex.eval(this.head.mounted_express,{vc:this,head:this.head})
+    }
     this.absImageUrl()
   },
     watch:{
       outvalue(nv){
+           alert('bb')
           this.absImageUrl()
       }
     },
@@ -64,6 +68,11 @@ export default {
   methods:{
     async absImageUrl(){
       var imageurl = this.row[this.head.name]
+      if(!imageurl){
+        this.abs_url = imageurl
+        return
+      }
+
       if(imageurl.startsWith('http')){
         this.abs_url = imageurl
       }else if(this.head.cdn){
