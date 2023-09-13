b93049e603569ce32a57d8edbc9bca851f33b52d
coblanplus <coblanplus@gmail.com>
2023-09-13 17:51:49 +0800

    fhj

--- func/myos.py
+++ func/myos.py
@@ -1,6 +1,8 @@
 import subprocess
-
 import platform
+from django.conf import settings
+from helpers.func.dot_dict import read_dict_path
+
 import logging
 general_log = logging.getLogger('general_log')
 
@@ -10,27 +12,30 @@ def is_install(soft_name):
     #p.wait()  
     stdout,erroutput = p.communicate()
     system_name = platform.system().lower()
-    general_log.debug(system_name)
+    if getattr(settings,'VERBOSE_LOG',False):
+        general_log.debug(system_name)
     
     try:
         if system_name == 'windows':
-            print("system = windows")
+            #print("system = windows")
             #ww = p.stdout.read()
    
             #general_log.debug(ww)
             ww= stdout.decode('gbk')
-            general_log.debug(ww)
+            if getattr(settings,'VERBOSE_LOG',False):
+                general_log.debug(ww)
             if '不是内部或外部命令' in ww:
                 return False
             else:
                 return True
            
         elif system_name == 'linux':
-            print('system = linux')
+            #print('system = linux')
             #ww = p.stdout.read()
             #general_log.debug(ww)  
             ww= stdout.decode('utf-8') 
-            general_log.debug(ww) 
+            if getattr(settings,'VERBOSE_LOG',False):
+                general_log.debug(ww) 
             if 'not found' in ww:
                 return False
             else:
