01485900dc6cbcf3cb3eb562ea5d21944bd61924
coblanplus <coblanplus@gmail.com>
2023-09-25 10:43:51 +0800

    dfh

--- case/jb_admin/js/field_editor/jsonEdit.vue
+++ case/jb_admin/js/field_editor/jsonEdit.vue
@@ -6,29 +6,68 @@
 <script>
 export default {
   props:['row','head'],
-  mounted(){
-    var self = this
-    ex.load_css(cfg.js_lib.jsoneditor_css)
-    ex.load_js(cfg.js_lib.jsoneditor).then(()=>{
-      if(self.head.readonly){
-        var options = {
-          mode: 'view',
-          // modes: ['code', 'form', 'text', 'tree', 'view', 'preview']
-          modes: [ 'view', 'preview']
-        }
-      }else{
-        var options = {
-          mode: 'code',
-          // modes: ['code', 'form', 'text', 'tree', 'view', 'preview']
-          modes: ['code', 'form', 'tree', 'view', 'preview']
-        }
+  data(){
+      return {
+        inn_value:this.row[this.head.name]
       }
+  },
+  mounted(){
+    this.initData()
+    var par = ex.vueParStore(this)
+    par.vc.before_submit.push(this.updateData )
+  },
+  computed:{
+      out_value(){
+        return this.row[this.head.name]
+      }
+  },
+  watch:{
+    out_value(nv){
+        this.inn_value = this.row[this.head.name]
+        this.updateEditorData()
+    }
+  },
+  methods:{
+    initData(){
+      var self = this
+      ex.load_css(cfg.js_lib.jsoneditor_css)
+      ex.load_js(cfg.js_lib.jsoneditor).then(()=>{
+        if(self.head.readonly){
+          var options = {
+            mode: 'view',
+            // modes: ['code', 'form', 'text', 'tree', 'view', 'preview']
+            modes: [ 'view', 'preview']
+          }
+        }else{
+          var options = {
+            mode: 'code',
+            // modes: ['code', 'form', 'text', 'tree', 'view', 'preview']
+            modes: ['code', 'form', 'tree', 'view', 'preview']
+          }
+        }
 
-      var value = this.row[this.head.name]
+        var value =  this.inn_value  //this.row[this.head.name]
 
-      this. editor = new JSONEditor(this.$el.querySelector('.editor'), options)
+        this. editor = new JSONEditor(this.$el.querySelector('.editor'), options)
 
-      // set json
+        // set json
+        if(!value){
+          var initialJson = ex.eval(this.head.default_value_express,{vc:this})  || {}
+        } else if(typeof value =='string'){
+          var initialJson = JSON.parse(this.row[this.head.name])
+        }else{
+          var initialJson = value
+        }
+
+        this.editor.set(initialJson)
+
+        // get json
+        // const updatedJson = editor.get()
+      })
+    },
+    updateEditorData(){
+      var value =  this.inn_value  //this.row[this.head.name]
+
       if(!value){
         var initialJson = ex.eval(this.head.default_value_express,{vc:this})  || {}
       } else if(typeof value =='string'){
@@ -38,15 +77,7 @@ export default {
       }
 
       this.editor.set(initialJson)
-
-      // get json
-      // const updatedJson = editor.get()
-    })
-
-    var par = ex.vueParStore(this)
-    par.vc.before_submit.push(this.updateData )
-  },
-  methods:{
+    },
     updateData(){
       this.row[this.head.name] = JSON.stringify( this.editor.get() )
     }
