0fa9688d9a2e90ab03376a591bc45e9b84eae684
coblanplus <coblanplus@gmail.com>
2023-09-28 17:36:43 +0800

    auto commit

---
+++ case/jb_admin/js/filter_editor/dateRange.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="com-filter-date-range">
+    <datetimeRange :head="head" :search_args="search_args">
+      <template v-slot:start-input="def_props">
+           <com-field-date :head="def_props.heads[0]" :row.sync="search_args"></com-field-date>
+      </template>
+      <template v-slot:end-input="def_props">
+        <com-field-date :head="def_props.heads[1]" :row.sync="search_args"></com-field-date>
+      </template>
+    </datetimeRange>
+  </div>
+</template>
+<script>
+import datetimeRange from "./datetimeRange.vue";
+export default {
+  props:['head','search_args'],
+  components:{
+    datetimeRange
+  }
+}
+</script>
