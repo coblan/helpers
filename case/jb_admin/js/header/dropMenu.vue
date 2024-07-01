950293df6d6ecb6c7c43d0020a5ea3db7ce0973d
coblanplus <coblanplus@gmail.com>
2024-07-01 14:51:59 +0800

    改进dropdown menu

--- case/jb_admin/js/header/dropMenu.vue
+++ case/jb_admin/js/header/dropMenu.vue
@@ -1,27 +1,34 @@
 <template>
   <li class="dropdown messages-menu">
+
+    <el-dropdown  trigger="click">
+        <div class="el-dropdown-link my-drop-down">
+          {{ctx.label}}<i class="el-icon-arrow-down el-icon--right"></i>
+        </div>
+<!--      <a href="#" class="dropdown-toggle" data-toggle="dropdown">-->
+<!--        &lt;!&ndash;      <i class="fa fa-envelope-o"></i>&ndash;&gt;-->
+<!--        &lt;!&ndash;      <span class="label label-success">4</span>&ndash;&gt;-->
+<!--        <i class="fa fa-user-circle-o"></i>-->
+<!--        <span>{{ctx.label}}</span>-->
+<!--      </a>-->
+
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item v-for="action in ctx.options" @click="runAction(action)">{{action.label}}</el-dropdown-item>
+        </el-dropdown-menu>
+    </el-dropdown>
+
+
     <!-- Menu toggle button -->
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-<!--      <i class="fa fa-envelope-o"></i>-->
-<!--      <span class="label label-success">4</span>-->
-      <i class="fa fa-user-circle-o"></i>
-      <span>{{ctx.label}}</span>
-    </a>
-    <ul class="dropdown-menu">
-      <li class="my-row" v-for="action in ctx.options" @click="runAction(action)">{{action.label}}</li>
+<!--    <a href="#" class="dropdown-toggle" data-toggle="dropdown">-->
+<!--&lt;!&ndash;      <i class="fa fa-envelope-o"></i>&ndash;&gt;-->
+<!--&lt;!&ndash;      <span class="label label-success">4</span>&ndash;&gt;-->
+<!--      <i class="fa fa-user-circle-o"></i>-->
+<!--      <span>{{ctx.label}}</span>-->
+<!--    </a>-->
+<!--    <ul class="dropdown-menu">-->
+<!--      <li class="my-row" v-for="action in ctx.options" @click="runAction(action)">{{action.label}}</li>-->
 
-<!--      <li>-->
-<!--&lt;!&ndash;         inner menu: contains the messages &ndash;&gt;-->
-<!--        <ul class="menu">-->
-
-<!--          &lt;!&ndash; end message &ndash;&gt;-->
-<!--        </ul>-->
-<!--        &lt;!&ndash; /.menu &ndash;&gt;-->
-
-
-<!--      </li>-->
-
-    </ul>
+<!--    </ul>-->
   </li>
 </template>
 <script>
@@ -48,6 +55,18 @@ export  default  {
     background-color: #ecf5ff;
     color: #66b1ff;
     cursor: pointer;
+  }
+}
+.my-drop-down{
+  color: white;
+  //padding-top: 15px;
+  //padding-bottom: 15px;
+  line-height: 20px;
+  padding: 15px;
+
+  &:hover{
+    background: rgba(0, 0, 0, 0.1);
+    color: #f6f6f6;
   }
 }
 </style>
