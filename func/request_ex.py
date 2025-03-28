57bff0c836abdce2523847787667fb93da63c0b5
coblanplus <coblanplus@gmail.com>
2025-03-28 14:57:57 +0800

    查看时间

--- func/request_ex.py
+++ func/request_ex.py
@@ -1,3 +1,6 @@
+from helpers.director.shortcut import get_request_cache
+import time
+
 def get_ip(request):
     '''获取请求者的IP信息'''
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')  # 判断是否使用代理
@@ -6,3 +9,14 @@ def get_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')  # 未使用代理获取IP
     return ip
+
+def request_time(name):
+    resp = get_request_cache()
+    if 'time_log' not in resp:
+        resp['time_log'] = [{'time':time.time(),'name':name,'elapse':0}]
+        return 0
+    else:
+        last = resp['time_log'][-1]
+        now = time.time()
+        resp['time_log'].append({'time':now,'name':name,'elapse':now-last['time']})
+        return resp['time_log']
