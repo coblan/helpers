6521a6c73faf92242e06f9e3057cb114e34d7640
coblan <coblan@163.com>
2025-06-09 13:10:19 +0800

    fsd

--- director/access/permit_api.py
+++ director/access/permit_api.py
@@ -5,8 +5,12 @@ from helpers.director.decorator import need_login
 
 @director_element('permit')
 class PermitApi(object):
+    public_api=['userPermit']
     @need_login
     def userPermit(self):
         user = get_request_cache()['request'].user
-        ls = list(set( user_permit_names(user) ) ) 
+        if user.is_superuser:
+            ls = ['is_superuser']
+        else:
+            ls = list(set( user_permit_names(user) ) ) 
         return ls
